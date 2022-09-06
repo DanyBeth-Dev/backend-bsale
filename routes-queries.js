2fe491348cc5a5e11772554aff5ae940950f3a8a
Daniela Hernández <105611396+DanyBeth-Dev@users.noreply.github.com>
2022-09-06 00:08:03 -0300

    ok

--- routes-queries.js
+++ routes-queries.js
@@ -3,26 +3,6 @@ const router = express.Router()
 
 const { pool } = require('./database.js')
 
-router.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html")
-})
-
-router.get('/bebida-energetica', (req, res) => res.sendFile(__dirname + "/index.html"))
-
-router.get('/pisco', (req, res) => res.sendFile(__dirname + "/index.html"))
-
-router.get('/ron', (req, res) => res.sendFile(__dirname + "/index.html"))
-
-router.get('/bebida', (req, res) => res.sendFile(__dirname + "/index.html"))
-
-router.get('/snack', (req, res) => res.sendFile(__dirname + "/index.html"))
-
-router.get('/cerveza', (req, res) => res.sendFile(__dirname + "/index.html"))
-
-router.get('/vodka', (req, res) => res.sendFile(__dirname + "/index.html"))
-
-router.get('/search/shows?=:query', (req, res) => res.sendFile(__dirname + "/index.html"))
-
 router.get('/data/', (req, res) => {
   pool.query('SELECT * FROM product', (err, rows, fields) => {
     if(!err) {
