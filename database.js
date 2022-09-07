aeadea5f5893774ca7dfa0722fd30415d1706a20
Daniela Hernández <105611396+DanyBeth-Dev@users.noreply.github.com>
2022-09-06 21:16:20 -0400

    ok

--- database.js
+++ database.js
@@ -5,9 +5,7 @@ const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  connectionLimit: 100000,
-  multipleStatements: true
+  database: process.env.DATABASE
 });
 
 
